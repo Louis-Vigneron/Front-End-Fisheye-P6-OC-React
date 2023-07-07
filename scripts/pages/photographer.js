b90bd2b3f8736856116ef54eae20de827928e674
Louis.V <loulou38101@gmail.com>
2023-07-07 18:43:13 +0200

    recovery data on photographer page and make header

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,57 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//Récupération du numéro de commande contenu dans l'URL
+
+const search_id = window.location.search;
+const Id = search_id.slice(1);
+
+fetch("../../data/photographers.json")
+
+    .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function (value) {
+        console.table(value);
+        displayPhotographer(value);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+
+
+function displayPhotographer(value) {
+
+    let photographer = [];
+
+    value.photographers.forEach(el => {
+        if (Id == el.id) {
+            photographer = el
+        }
+    });
+
+    let pictures = [];
+    value.media.forEach(el => {
+        if (Id == el.photographerId) {
+            pictures.push(el);
+        }
+    });
+
+    const headerPhotographe = document.querySelector('.photograph-header');
+    const portrait = photographer.portrait;
+    const picture = `assets/photographers/${portrait}`;
+
+    headerPhotographe.innerHTML =
+        `
+    <div>
+        <h2>${photographer.name}</h2>
+        <h3>${photographer.city}, ${photographer.country}</h3>
+        <p>${photographer.tagline}</p>
+    </div>    
+    <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+    <img src=${picture} alt=${photographer.name}>
+
+    `
+        ;
+
+
+}
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,29 +1,32 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+
+        const a = document.createElement('a');
+        a.href = `./photographer.html?${id}`;
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
         img.alt = name;
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        const pCity = document.createElement( 'h3' );
+        const pCity = document.createElement('h3');
         pCity.textContent = `${city}, ${country}`;
-        const pTagline = document.createElement( 'p' );
+        const pTagline = document.createElement('p');
         pTagline.textContent = tagline;
-        const pPrice = document.createElement( 'p' );
+        const pPrice = document.createElement('span');
         pPrice.textContent = `${price}€/jour`;
-
+        a.appendChild(article);
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(pCity);
         article.appendChild(pTagline);
         article.appendChild(pPrice);
-        return (article);
+        return (a);
     }
-    return { name, portrait, city, country, tagline, price, getUserCardDOM }
+    return { name, portrait, city, country, tagline, price, id, getUserCardDOM }
 }
 
