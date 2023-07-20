b607e41be4cd368412f573f246364e5c13614c36
Louis.V <loulou38101@gmail.com>
2023-07-20 16:03:58 +0200

    change sort options

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -52,12 +52,15 @@ function displayPhotographer(value) {
         <img src=${picture} alt=${photographer.name}>
     </div>
     
-    <label class="sort" for="sort">Trier par</label>
-    <select class="sort-select" name="sort" id="sort-select">
-        <option value="Popularité">Popularité</option>
-        <option value="Date">Date</option>
-        <option value="Titre">Titre</option>
-    </select>
+    <div class="sort">
+        <h2 class="sort-title">Trier par</h2>
+        <button class="sort-button" id="sort-button" aria-haspopup="listbox" role="button" aria-expanded="false" onclick="displaySortOptions()">Popularité </button><i class="fa-solid fa-chevron-down"></i>
+        <ul class="sort-select" id="sort-select">
+            <li id="Popularité" class="sort-option sort-option-border" onclick="selectSortOption('Popularité')" role="option" aria-selected="false" aria-labelledby="sort-button">Popularité <i class="fa-solid fa-chevron-up"></i></li>
+            <li id="Date" class="sort-option sort-option-border" onclick="selectSortOption('Date')" role="option" aria-selected="false">Date</li>
+            <li id="Titre" class="sort-option" onclick="selectSortOption('Titre')" role="option" aria-selected="false">Titre</li>
+        </ul>
+    </div>
     <div class="images-photographer"></div>
     <div class="lightBox"></div>
     <div class="total-likes">   
@@ -66,8 +69,6 @@ function displayPhotographer(value) {
     </div>     
     
         `;
-
-
     contactForm.innerHTML = `
     <div class="modal">
     <header>
@@ -95,9 +96,40 @@ function displayPhotographer(value) {
     </form>
     
   </div>`
-    sort(pictures, firstName);
-    addPicture(firstName, pictures);
+    
+    addPicture(firstName, pictures);    
+}
 
+function displaySortOptions(){   
+    const listOptions = document.getElementById('sort-select');
+    const button = document.getElementById('sort-button');  
+    const arrowDown = document.querySelector('.fa-chevron-down');
+    const expanded = listOptions.getAttribute('aria-expanded') === 'true';
+    listOptions.setAttribute('aria-expanded', !expanded);
+    listOptions.style.display = expanded ? 'none' : 'block';   
+    button.style.display = "none"; 
+    arrowDown.style.display = "none"; 
+}
+
+function selectSortOption(sortValue){
+    const listOptions = document.getElementById('sort-select');
+    const options = document.querySelectorAll('[role="option"]');
+    const button = document.getElementById('sort-button'); 
+    const arrowDown = document.querySelector('.fa-chevron-down');
+    const expanded = listOptions.getAttribute('aria-expanded') === 'true';
+    listOptions.setAttribute('aria-expanded', !expanded);
+    options.forEach(el => {
+        let isSelected = el.id === sortValue;
+        el.setAttribute('aria-selected', isSelected);
+    });
+    const menuButton = document.getElementById('sort-button');
+    let selectedOption = listOptions.querySelector('[aria-selected="true"]').textContent;
+    console.log(selectedOption)
+    menuButton.textContent = selectedOption;    
+    listOptions.style.display = expanded ? 'none' : 'block';
+    button.style.display = "block"; 
+    arrowDown.style.display = "block";    
+    sort(sortValue) 
 }
 
 function addPicture(firstName, pictures) {
@@ -105,24 +137,24 @@ function addPicture(firstName, pictures) {
     images.innerHTML = "";
     pictures.forEach(element => {
         if (element.image) {
-            images.innerHTML += `<figure>
-        <img class="img-photographer" src="assets/${firstName}/${element.image}" alt=${element.title}>
-        <figcaption class="img-legend">${element.title}<span class="likes-number"> ${element.likes}<i class="fa-solid fa-heart"></i></span></figcaption>
+            images.innerHTML += `<figure >
+        <img tabindex="0" class="img-photographer" src="assets/${firstName}/${element.image}" alt=${element.title}>
+        <figcaption class="img-legend">${element.title}<span class="likes-number"> ${element.likes}<i tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>
     </figure>`;
         }
         else {
             images.innerHTML += `
         <figure>
-             <video class="img-photographer" controls width="350" height="300">
+             <video tabindex="0" class="img-photographer" controls width="350" height="300">
                  <source src="assets/${firstName}/${element.video}" type="video/mp4">                            
              </video>
-             <figcaption class="img-legend">${element.title}<span class="likes-number"> ${element.likes}<i class="fa-solid fa-heart"></i></span></figcaption>
+             <figcaption class="img-legend">${element.title}<span class="likes-number"> ${element.likes}<i tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>
          </figure>
      `;
         }
     });
     addLike(pictures);
-    displayLightBox(pictures, firstName);
+    displayLightBox(pictures, firstName);   
 }
 
 function addLike(pictures) {
@@ -135,15 +167,22 @@ function addLike(pictures) {
         likeBtn[y].addEventListener("click", () => {
             let a = b[y].likes + 1;
             b[y].likes = a;
-            likeNumber[y].innerHTML = `<span> ${a}<i class="fa-solid fa-heart"></i></span></figcaption>`;
+            likeNumber[y].innerHTML = `<span> ${a}<i tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>`;
             likeTotalUpdate.textContent = totalLikes(b)
         })
+        likeBtn[y].addEventListener("keydown",(e)=>{
+            if (e.key === "Enter") {
+                let a = b[y].likes + 1;
+                b[y].likes = a;
+                likeNumber[y].innerHTML = `<span> ${a}<i tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>`;
+                likeTotalUpdate.textContent = totalLikes(b)
+        }})
 
     };
 }
 
 function sort(pictures, firstName) {
-    let optionPick = document.querySelector("#sort-select");
+    let optionPick = document.querySelector("#sort-button");
     optionPick.addEventListener("change", () => {
         if (optionPick.value == "Popularité") {
             pictures.sort(byLikes);
@@ -215,22 +254,33 @@ function displayLightBox(pictures, firstName) {
             else {
                 displayVideoCarrousel(firstName, pictures, x)
             }
-        })
-    }
-
+        })      
+        picture[x].addEventListener("keydown",(e)=>{
+            if (e.key === "Enter") {
+                lightBox.style.display = "block";
+                totalLikesHide.style.display = "none";
+                if (pictures[x].image) {
+                    displayImageCarrousel(firstName, pictures, x)
+                }
+                else {
+                    displayVideoCarrousel(firstName, pictures, x)
+                }
+        }})
+    } 
+   
 }
 
 function displayImageCarrousel(firstName, pictures, x) {
     const lightBox = document.querySelector(".lightBox");
     lightBox.innerHTML = `       
             <div class="nextTo">
-               <img id="left" src="assets/left.svg" alt="left cross">
+               <img tabindex="0" id="left" src="assets/left.svg" alt="left cross">
                 <figure class="figure-carrousel">
                     <img class="img-carrousel" src="assets/${firstName}/${pictures[x].image}" alt=${pictures[x].title}>
                     <figcaption class="img-legend-carrousel">${pictures[x].title}</figcaption>
                 </figure>
-                <img id="right" src="assets/right.svg" alt="right cross">
-                <div class="cross">
+                <img tabindex="0" id="right" src="assets/right.svg" alt="right cross">
+                <div tabindex="0" class="cross">
                     <div class="cross1"></div>
                     <div class="cross2"></div>
                 </div>
@@ -244,15 +294,15 @@ function displayVideoCarrousel(firstName, pictures, x) {
     const lightBox = document.querySelector(".lightBox");
     lightBox.innerHTML = `
     <div class="nextTo">
-        <img id="left" src="assets/left.svg" alt="left cross">
+        <img tabindex="0" id="left" src="assets/left.svg" alt="left cross">
         <figure class="figure-carrousel">
             <video class="img-carrousel" controls width="350" height="300">
                 <source src="assets/${firstName}/${pictures[x].video}" type="video/mp4">                            
             </video>
             <figcaption class="img-legend-carrousel">${pictures[x].title}</figcaption>
         </figure>
-        <img id="right" src="assets/right.svg" alt="right cross">
-        <div class="cross">
+        <img tabindex="0" id="right" src="assets/right.svg" alt="right cross">
+        <div tabindex="0" class="cross">
             <div class="cross1"></div>
             <div class="cross2"></div>
         </div>
@@ -317,3 +367,7 @@ function nextPicture(firstName, pictures, index) {
         displayVideoCarrousel(firstName, pictures, index)
     }
 }
+
+
+
+//https://achecks.org/achecker/
