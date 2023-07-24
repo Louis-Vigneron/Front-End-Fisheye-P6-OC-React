195f4370b062c0b0067d16434f0119a5b175cc84
Louis.V <loulou38101@gmail.com>
2023-07-24 16:00:23 +0200

    minor fix

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -82,24 +82,24 @@ function displayPhotographerPage(value) {
     <div class="modal">
     <header>
       <h2>Contactez-moi <br> ${photographer.name}</h2>
-      <img class="modal-close" src="assets/icons/close.svg" tabindex="0" onclick="closeModal()" onkeypress="handleKeyPress(event)" />
+      <img class="modal-close" src="assets/icons/close.svg" tabindex="0" onclick="closeModal()" onkeypress="handleKeyPress(event)" alt="Bouton fermer" />
     </header>
     <form method="get" >
       <div class="modal__form">
-        <label class="modal__label" for="firstName">Prénom</label><p  class="modal__error" id="firstNameErrorMsg"></p>
-        <input id="first"  name="first"/>
+        <label class="modal__label" for="prénom">Prénom</label><p  class="modal__error" id="firstNameErrorMsg"></p>
+        <input type="text" id="first" name="prénom" aria-labelledby="Prénom"/>
       </div>
       <div class="modal__form">
-        <label class="modal__label"  for="lastName">Nom</label><p  class="modal__error" id="lastNameErrorMsg"></p>
-        <input id="last"  name="last"/>
+        <label class="modal__label"  for="nom">Nom</label><p  class="modal__error" id="lastNameErrorMsg"></p>
+        <input type="text" id="last" name="nom" aria-labelledby="Nom"/>
       </div>
       <div class="modal__form">
         <label class="modal__label"  for="email">Email</label><p  class="modal__error" id="emailErrorMsg"></p>
-        <input id="email"  name="email" />
+        <input type="email" id="email" name="email" aria-labelledby="Email"/>
       </div>
       <div class="modal__form">
         <label class="modal__label"  for="message">Votre message</label><p id="messageErrorMsg" class="modal__error"></p>
-        <input id="message"  name="message" class="message-modal" />
+        <input type="text" id="message" name="message" class="message-modal" aria-labelledby="Votre message"/>
       </div>
       <button class="contact_button send-button" type="button" onclick="sendModal()">Envoyer</button>
     </form>
@@ -299,15 +299,15 @@ function displayImageCarrousel(pictures, x, firstName) {
     const lightBox = document.querySelector(".lightBox");
     lightBox.innerHTML = `       
             <div class="nextTo">
-            <button class="button-carrousel" id="left"><img  src="assets/left.svg" alt="left cross"></button>
+            <button class="button-carrousel" id="left"><img  src="assets/left.svg" alt="flèche de gauche"></button>
                
                 <figure class="figure-carrousel">
                     <img class="img-carrousel" src="assets/${firstName}/${pictures[x].image}" alt=${pictures[x].title}>
                     <figcaption class="img-legend-carrousel">${pictures[x].title}</figcaption>
                 </figure>
-                <button class="button-carrousel" id="right"><img  src="assets/right.svg" alt="right cross"></button>
+                <button class="button-carrousel" id="right"><img  src="assets/right.svg" alt="flèche de droite"></button>
                 
-                <div tabindex="0" class="cross">
+                <div tabindex="0" aria-label="Bouton fermer lightbox" class="cross">
                 <div class="cross1"></div>
                 <div class="cross2"></div>
             </div>
@@ -323,15 +323,15 @@ function displayVideoCarrousel(pictures, x, firstName) {
     const lightBox = document.querySelector(".lightBox");
     lightBox.innerHTML = `
     <div class="nextTo">
-    <button class="button-carrousel" id="left"><img  src="assets/left.svg" alt="left cross"></button>
+    <button class="button-carrousel" id="left"><img  src="assets/left.svg" alt="flèche de gauche"></button>
         <figure class="figure-carrousel">
             <video class="img-carrousel" controls width="350" height="300">
                 <source src="assets/${firstName}/${pictures[x].video}" type="video/mp4">                            
             </video>
             <figcaption class="img-legend-carrousel">${pictures[x].title}</figcaption>
         </figure>
-        <button class="button-carrousel" id="right"><img  src="assets/right.svg" alt="right cross"></button>
-        <div tabindex="0" class="cross">
+        <button class="button-carrousel" id="right"><img  src="assets/right.svg" alt="flèche de droite"></button>
+        <div tabindex="0" aria-label="Bouton fermer lightbox" class="cross">
             <div class="cross1"></div>
             <div class="cross2"></div>
         </div>
--- scripts/templates/pictures.js
+++ scripts/templates/pictures.js
@@ -20,7 +20,7 @@ class PictureCard {
         const pictureCard = `
         <figure >
             <img tabindex="0" class="img-photographer" src="assets/${this.picture.FirstName}/${this.picture.Image}" alt=${this.picture.Title}>
-            <figcaption class="img-legend">${this.picture.Title}<span class="likes-number"> ${this.picture.Likes}<i tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>
+            <figcaption class="img-legend">${this.picture.Title}<span class="likes-number"> ${this.picture.Likes}<i aria-label="Bouton j'aime" tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>
         </figure>
     `
     return pictureCard
@@ -53,7 +53,7 @@ class VideoCard {
              <video tabindex="0" class="img-photographer" controls width="350" height="300">
                  <source src="assets/${this.data.FirstName}/${this.data.Video}" type="video/mp4">                            
              </video>
-             <figcaption class="img-legend">${this.data.Title}<span class="likes-number"> ${this.data.Likes}<i tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>
+             <figcaption class="img-legend">${this.data.Title}<span class="likes-number"> ${this.data.Likes}<i aria-label="Bouton j'aime" tabindex="0" class="fa-solid fa-heart"></i></span></figcaption>
          </figure>
     `
     return videoCard
