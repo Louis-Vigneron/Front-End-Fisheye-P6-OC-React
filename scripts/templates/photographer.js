ef74cb3d525df024e05b4cc5292f9e0b5911f25b
Louis.V <loulou38101@gmail.com>
2023-07-24 15:18:34 +0200

    fix country photographer

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -15,14 +15,14 @@ class PhotographerCard {
     constructor(photographer) {
         this.photographer = photographer
     }
-
-    createPhotographerCard() {
+   
+    createPhotographerCard() {       
         const photographerCard = `
             <a href="./photographer.html?${this.photographer.Id}">
                 <article>
                     <img src="assets/photographers/${this.photographer.Portrait}" alt="${this.photographer.Name}">
                     <h2>${this.photographer.Name}</h2>
-                    <h3>${this.photographer.City}, ${this.Country}</h3>
+                    <h3>${this.photographer.City}, ${this.photographer.Country}</h3>
                     <p>${this.photographer.TagLine}</p><span>${this.photographer.Price}€/jour</span>
                 </article>
             </a>
